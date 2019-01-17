66c65ae259961194bf8bc2b43df4e4c55b88f454
unknown <2158118@labinfo.is.escuelaing.edu.co>
2019-01-17 08:44:50 -0500

    Ejercicio A

--- laboratorios/1-recursion/A/problem.py
+++ laboratorios/1-recursion/A/problem.py
@@ -1,9 +1,17 @@
 import json
-
+def gir(text, cont,rev):
+    if cont==-1:
+        return rev
+    else:
+        rev=rev+str(text[cont])
+        return gir(text,cont-1,rev)
+    
 
 # TODO Complete!!
 def reverse(text):
-    return text
+    rev=""
+    t=gir(text,len(text)-1,rev)
+    return t
 
 
 if __name__ == '__main__':
