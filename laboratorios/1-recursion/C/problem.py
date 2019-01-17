2ff7b0eb17b89a3b88edb2bfb1d100322dfcb66a
unknown <2158118@labinfo.is.escuelaing.edu.co>
2019-01-17 09:57:06 -0500

    Ejercicio C

--- laboratorios/1-recursion/C/problem.py
+++ laboratorios/1-recursion/C/problem.py
@@ -1,9 +1,26 @@
 import json
 
-
+def rev(text,cv,cc,cont,voc):
+    if cont==len(text):
+        if cv>cc:
+            return True
+        elif cv<cc or cc==cv:
+            return False
+    else:
+        for i in range(len(voc)):
+            if text[cont]==voc[i]:
+                cv=cv+1
+            if i==4:
+                if text[cont]!="a" and text[cont]!="e" and text[cont]!="i" and text[cont]!="o" and text[cont]!="u":
+                    cc=cc+1
+        return rev(text,cv,cc,cont+1,voc)
+                
 # TODO Complete!
 def has_more_vowels(s):
-    return False
+    s=s.lower()
+    voc=["a","e","i","o","u"]
+    c=rev(s,0,0,0,voc)
+    return c
 
 
 if __name__ == '__main__':
